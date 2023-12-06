77df03a1312267e7c32a969bb8a8045be841cc2b
zayed1611 <zayedrohan.cnits@gmail.com>
2023-12-06 11:49:45 +0600

    6th commit

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -10,3 +10,23 @@ $(window).scroll(function(){
     $("html, body").animate({ scrollTop: 0 }, 2000);
     return false;
   });
+
+
+
+  $("#slider2").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
