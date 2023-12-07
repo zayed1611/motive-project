b439a3c93dc44e965c45f1fb339a6a0e1d4c3c1e
zayed1611 <zayedrohan.cnits@gmail.com>
2023-12-07 17:12:09 +0600

    11th commit

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -32,3 +32,35 @@ $(window).scroll(function(){
 });
 
 
+var typed2 = new Typed('.text-2', {
+  strings: [' ZAYED AMIN',],
+  typeSpeed: 100,
+  backSpeed: 1000,
+  fadeOut: true,
+  loop: true
+}); 
+
+// var typed2 = new Typed('.text', {
+//   strings: ['Web Designer', 'Frontend Designer', 'Youtuber'],
+//   typeSpeed: 100,
+//   backSpeed: 1000,
+//   fadeOut: true,
+//   loop: true
+// });
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 10000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
